734983d5959bf74f569681c39443ffdfca8baac3
Soniya Patel <soniya.patel@infostretch.com>
2019-09-02 16:11:09 +0530

    created javascript object and save into json file, read that json file converted into object and get value from it

--- playground/json.js
+++ playground/json.js
@@ -1,12 +1,24 @@
-var obj = {
-  name: 'Soniya'
-};
+// var obj = {
+//   name: 'Soniya'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+//
+// var personString = '{"name": "soniya", "age": 28}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+const fs = require('fs');
+var originalNotes = {
+  title: "some title",
+  body: "some body"
+}
 
-var personString = '{"name": "soniya", "age": 28}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+fs.writeFileSync('notes.json', JSON.stringify(originalNotes));
+var notes = fs.readFileSync('notes.json');
+var notesObject = JSON.parse(notes);
+console.log(typeof notesObject);
+console.log(notesObject.title);
