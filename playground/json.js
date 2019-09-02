e204f156c8c91475c00d57c6c807a4634a38e063
Soniya Patel <soniya.patel@infostretch.com>
2019-09-02 15:38:10 +0530

    converted jsong string to json object using JSON.parse

--- playground/json.js
+++ playground/json.js
@@ -5,3 +5,8 @@ var obj = {
 var stringObj = JSON.stringify(obj);
 console.log(typeof stringObj);
 console.log(stringObj);
+
+var personString = '{"name": "soniya", "age": 28}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
