ea6ad63c343b6f136b0b49ad7011452123cc597b
Soniya Patel <soniya.patel@infostretch.com>
2019-09-05 23:33:40 +0530

    learned difference between arrow function and normal function

---
+++ playground/arrow-func.js
@@ -0,0 +1,19 @@
+var square = (x) => {
+  return x*x;
+}
+console.log(square(2));
+
+var user = {
+  name: 'soniya',
+  setHi:() => {
+    // arrow function is not allowing this key word
+    //output of below line: Hello   undefined
+    console.log(`Hello   ${this.name}`);
+  },
+  setHiAlt () {
+    console.log(`Hello   ${this.name}`);
+    // output of above line: Hello   soniya
+  }
+}
+
+user.setHiAlt();
